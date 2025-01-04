21855b0f8cb05f189606cd3eb5bcc4138b08ce28
Anner Escobar <anner123escobar@gmail.com>
2025-01-04 11:38:26 -0600

    configurando estructura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddCaseComponent } from "./casos/pages/add-case/add-case.component";
+import { TypeCaseComponent } from "./casos/components/type-case/type-case.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddCaseComponent, TypeCaseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/casos/dashboard-case/dashboard-case.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-case',
+  imports: [],
+  templateUrl: './dashboard-case.component.html',
+  styleUrl: './dashboard-case.component.css'
+})
+export class DashboardCaseComponent {
+
+}
