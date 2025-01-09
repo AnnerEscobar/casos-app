dcd9989e4dbdfd1243900ea467470d2f4cb937c9
Anner Escobar <anner123escobar@gmail.com>
2025-01-08 20:34:49 -0600

    pagina de perfil lista

--- src/app/busquedas/profile/profile.component.ts
+++ src/app/busquedas/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
