6459556709b250bf2a036af5a60bcb318293d6c5
Anner Escobar <anner123escobar@gmail.com>
2025-01-09 12:29:29 -0600

    actualizando cambios

--- src/app/busquedas/search-profile/search-profile.component.ts
+++ src/app/busquedas/search-profile/search-profile.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-search-profile',
-  imports: [],
+  imports: [
+    MatCheckboxModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatDividerModule,
+    MatCardModule
+  ],
+  providers:[
+    provideNativeDateAdapter()
+  ],
+
   templateUrl: './search-profile.component.html',
   styleUrl: './search-profile.component.css'
+
 })
 export default class SearchProfileComponent {
 
