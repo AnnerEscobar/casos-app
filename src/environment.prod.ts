86394fe35de6cd902fa1e0517c12f1d51d761b61
Anner Escobar <anner123escobar@gmail.com>
2025-04-29 00:29:32 -0600

    subiendo cambiso:

--- src/environment.prod.ts
+++ src/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://casos-app-backend-production.up.railway.app'
-  /* apiUrl: 'http://localhost:3000' */
+  /* apiUrl: 'thhp://localhost:3000' */
 };
