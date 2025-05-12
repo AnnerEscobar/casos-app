32df86de656fe72ae7e4d94a0a76dcab17f5ad61
Anner Escobar <anner123escobar@gmail.com>
2025-05-11 20:06:27 -0600

    el sidenav por default estara cerrado

--- src/app/casos/dashboard-case.component.ts
+++ src/app/casos/dashboard-case.component.ts
@@ -20,7 +20,7 @@ import { CommonModule } from '@angular/common';
 })
 export default class DashboardCaseComponent {
   events: string[] = [];
-  opened: boolean = true;
+  opened: boolean = false;
 
   constructor(){
     console.log(this.events)
