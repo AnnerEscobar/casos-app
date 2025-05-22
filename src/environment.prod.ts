48e7504b2f6d5a25dbecf10302b59deb92f5e97c
Anner Escobar <anner123escobar@gmail.com>
2025-05-21 19:25:28 -0600

    comimit para cambiar de rama

--- src/environment.prod.ts
+++ src/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://casos-app-backend-production.up.railway.app'
-   /* apiUrl: 'http://localhost:3000'/*
-   apiUrl: 'https://7k1k7txz-3000.use2.devtunnels.ms'*/
+   /* apiUrl: 'http://localhost:3000' */
+
+   /* apiUrl: 'https://7k1k7txz-3000.use2.devtunnels.ms'*/
 };
